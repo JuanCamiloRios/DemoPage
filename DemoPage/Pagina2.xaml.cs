5844371e2f1922db167fb78545f8fe7d0c6b1ea7
CarlosAndres <andresitomf4@gmail.com>
2018-08-30 15:45:55 -0500

    paginas

--- DemoPage/Pagina2.xaml.cs
+++ DemoPage/Pagina2.xaml.cs
@@ -11,5 +11,31 @@ namespace DemoPage
         {
             InitializeComponent();
         }
+
+        private async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            this.Label.Text = string.Empty;
+            await Navigation.PushAsync(new Pagina3());
+
+        }
+
+        private async void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            this.Label.Text = string.Empty;
+            var resp = await DisplayAlert("Test", "Esta seguro de realizar el test", "si", "no");
+            if (resp)
+            {
+
+                await DisplayAlert("Inicio de test", "Super Bienvenido", "Aceptar");
+                var opcion = await DisplayActionSheet("Opciones", "Cancelar", null, "Opcion 1", "Opcion2", "Opcion 3");
+                Label.Text = opcion.ToString();
+            }
+            else
+            {
+                await DisplayAlert("Test fallido", "En otra ocasión sera", "Aceptar");
+
+            }
+        }
+
     }
 }
---
+++ DemoPage/Pagina3.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DemoPage
+{
+    public partial class Pagina3 : ContentPage
+    {
+        public Pagina3()
+        {
+            InitializeComponent();
+        }
+
+        void Btn1_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Btn2_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Btn3_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
